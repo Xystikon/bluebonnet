48654be5b5d51c09a31d5e8878aae9d554bbc831
Xystikon <haroon@amanullah.org>
2019-01-14 17:12:53 -0600

    new bluebonnet style

--- walters-house.py
+++ walters-house.py
@@ -2,37 +2,41 @@ import sys
 from time import sleep
 import random
 
-n=0
-ne=0
-e=0
-se=0
-s=0
-sw=0
-w=0
-nw=0
+def init():
 
-bedroom=1
-hallway=1
-bathroom=1
-room=1
-stairtop=1
-couch=1
+        n=0
+        ne=0
+        e=0
+        se=0
+        s=0
+        sw=0
+        w=0
+        nw=0
+        
+        bedroom=1
+        hallway=1
+        bathroom=1
+        room=1
+        stairtop=1
+        couch=1
+        
+        stairbottom=1
+        dineroom=1
+        office=1
+        kitchen=1
+        liveroom=1
+        hallway2=1
+        door=1
+        
+        l=0
+        
+        currentloc = "bedroom"
+        print("I wake up in a bed, cold and hard. I sit up and walk to the middle of the room.")
 
-stairbottom=1
-dineroom=1
-office=1
-kitchen=1
-liveroom=1
-hallway2=1
-door=1
+        start()
 
-l=0
-
-currentloc = "bedroom"
-print("I wake up in a bed, cold and hard. I sit up and walk to the middle of the room.")
-
-while 1!=0:	
-	print("")
+def start():
+        print("")
 	
 	if currentloc == "bedroom":
 		n="window"
@@ -45,7 +49,6 @@ while 1!=0:
 		nw="wall"
 		
 		print("Bedroom\n")
-		
 		if bedroom == 1 or l == 1:
 			print("There are two beds in the room, one of which I woke up in. To the north of me is a window that is boarded up, and there is a single exit through a door to the west.")
 			bedroom = 0
@@ -62,7 +65,6 @@ while 1!=0:
 		nw="couch"
 		
 		print("Hallway\n")
-		
 		if hallway == 1 or l == 1:
 			print("I am in a hallway leading from east and west. A picture of an unknown family is north of me, and is mostly covered in moss. A doorway to a bedroom is east of me, and to the north is a closed door.")
 			hallway = 0
@@ -79,7 +81,6 @@ while 1!=0:
 		nw="wall"
 		
 		print("Bathroom\n")
-		
 		if bathroom == 1 or l == 1:
 			print("I am now in a bathroom, with a sink north of me and a toilet east of me. I can exit the bathroom south.")
 			bathroom = 0
@@ -96,7 +97,6 @@ while 1!=0:
 		nw="wall"
 	
 		print("Room\n")
-	
 		if room == 1 or l == 1:
 			print("The hallway to the west opens up into a small room, in which there is a couch north. Going west is prevented due to a barred door. To the south is the top of a staircase leading down into the darkness.")
 			room = 0
@@ -113,7 +113,6 @@ while 1!=0:
 		nw="wall"
 		
 		print("Other Side of the Room\n")
-		
 		if couch == 1 or l == 1:
 			print("I am now on the other side of the room, a couch east of me. A smashed out tv in to my west. The other side of the room is south of me.")
 			couch = 0
@@ -130,14 +129,11 @@ while 1!=0:
 		nw="hallway"
 		
 		print("The Top of a Staircase\n")
-		
 		if stairtop == 1 or l == 1:
 			print("I am at the top of a staircase, leading into darkness. To the north goes back to the room opening, and to the east is the staircase.")
 			stairtop = 0
 			l=0
-			
-			
-			
+					
 	elif currentloc == "stairbottom":
 		n="door"
 		ne="dineroom"
@@ -149,7 +145,6 @@ while 1!=0:
 		nw="wall"
 		
 		print("The Bottom of a Staircase\n")
-		
 		if stairbottom == 1 or l == 1:
 			print("I am at the bottom of a staircase, leading upwards to the west. A door is infront of me, and a dining room is northwest. A kitchen is towards the east and a living room is south of me.")
 			stairbottom = 0
@@ -165,8 +160,7 @@ while 1!=0:
 		w="wall"
 		nw="wall"
 		
-		print("The Door\n")
-		
+		print("The Door\n")		
 		if door == 1 or l == 1:
 			print("I come to a stop at a door, slightly ajar. To the south goes backwards to the staircase.")
 			door = 0
@@ -199,34 +193,37 @@ while 1!=0:
 		w="stairbottom"
 		nw="wall"
 		
-		print("Kitchen\n")
-		
+		print("Kitchen\n")		
 		if kitchen == 1 or l == 1:
 			print("I am in a kithcen, a balcony north of me. I can go back to the bottom of the stairs by walking west.")
 			kitchen = 0
 			l=0
 
-	action = input("")
-	
-	if action == "n":
+
+        action = input('\n')
+        move(action)
+
+def move(act):
+        
+        if action == "n":
 		currentloc = n
-    elif action == "ne":
-        currentloc = ne
+        elif action == "ne":
+                currentloc = ne
 	elif action == "e":
 		currentloc = e
-    elif action == "se":
-        currentloc = se
+        elif action == "se":
+                currentloc = se
 	elif action == "s":
 		currentloc = s
-    elif action == "sw":
-        currentloc = sw
+        elif action == "sw":
+                currentloc = sw
 	elif action == "w":
 		currentloc = w
-    elif action == "nw":
-        currentloc = nw
+        elif action == "nw":
+                currentloc = nw
 
-    elif action == "l":
-        l = 1
+        elif action == "l":
+                l = 1
 		
 	if e == "wall" and currentloc == e or s == "wall" and currentloc == s or w == "wall" and currentloc == w or n == "wall" and currentloc == n:
 		wallr = random.randint(0, 2)
@@ -248,4 +245,4 @@ while 1!=0:
 		if windowr == 2:
 			print("I can't get past this window. Or see out of it.")
 
-	currentlocsplit = currentloc.split()
+        
